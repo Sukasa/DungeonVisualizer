9ae7c1821e8b0a248e823d372e91bdf7d06de93a
Sukasa <nick.m@shaw.ca>
2016-12-03 14:26:10 -0800

    Use more generators and try new things

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,20 +31,27 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.DungeonSize = new System.Windows.Forms.NumericUpDown();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DungeonSize)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(391, 258);
+            this.pictureBox1.Size = new System.Drawing.Size(504, 453);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 276);
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(138, 471);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
@@ -54,24 +61,47 @@
             // 
             // label1
             // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(93, 281);
+            this.label1.Location = new System.Drawing.Point(219, 476);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(156, 13);
             this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.label1.Text = "Select a size and hit \'Generate\'!";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // DungeonSize
+            // 
+            this.DungeonSize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.DungeonSize.Location = new System.Drawing.Point(12, 473);
+            this.DungeonSize.Maximum = new decimal(new int[] {
+            1024,
+            0,
+            0,
+            0});
+            this.DungeonSize.Name = "DungeonSize";
+            this.DungeonSize.Size = new System.Drawing.Size(120, 20);
+            this.DungeonSize.TabIndex = 3;
+            this.DungeonSize.Value = new decimal(new int[] {
+            256,
+            0,
+            0,
+            0});
+            this.DungeonSize.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(415, 448);
+            this.ClientSize = new System.Drawing.Size(528, 503);
+            this.Controls.Add(this.DungeonSize);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Dungeon Generator Test Harness";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DungeonSize)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -82,6 +112,7 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown DungeonSize;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,14 +1,9 @@
-﻿using Dungeon_Generator;
-using Dungeon_Generator.Generators;
+﻿using DungeonGenerator;
+using DungeonGenerator.Generators;
+using DungeonGenerator.Transformers;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Drawing.Imaging;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace DungeonVisualizer
@@ -20,34 +15,189 @@ namespace DungeonVisualizer
             InitializeComponent();
         }
 
+        private enum CityTile : int
+        {
+            Grass,
+            Road,
+            Building,
+            Wall,
+            TreeTrunk,
+            Foliage,
+            Water
+        }
+
         private static Color[] Palette = new Color[] { Color.Black, Color.LightGray, Color.White };
         private void button1_Click(object sender, EventArgs e)
         {
-            DungeonFloor<int> Dungeon = new DungeonFloor<int>(256);
+            DungeonFloor<int> Dungeon = new DungeonFloor<int>((int)DungeonSize.Value);
             CityRoadsGenerator<int> Generator = new CityRoadsGenerator<int>(Dungeon);
             Generator.DefaultBlockedTile = 0;
             Generator.DefaultOpenTile = 1;
             Generator.DefaultFeatureTile = 2;
-
+            int Attempts = 0;
             do
             {
                 Generator.Generate();
-            } while (Generator.Roads < 40 || Dungeon.EdgePierced(Generator.DefaultBlockedTile));
+                Attempts++;
+            } while (Dungeon.EdgePierced(Generator.DefaultBlockedTile));
+
+            DungeonFloor<Color> ColourMap = new DungeonFloor<Color>(Dungeon.Size);
+            DungeonFloor<CityTile> TileMap = new DungeonFloor<CityTile>(Dungeon.Size);
+
+            LambdaTransformer<int, CityTile> TileTransformer = new LambdaTransformer<int, CityTile>(Dungeon, TileMap);
+            LambdaTransformer<CityTile, CityTile> FoliageTransformer = new LambdaTransformer<CityTile, CityTile>(TileMap, TileMap);
+            LambdaTransformer<CityTile, Color> MapTransformer = new LambdaTransformer<CityTile, Color>(TileMap, ColourMap);
+
+            //--------------
+            // Do the tile transforms
+
+            // Roads
+            TileTransformer.Lambdas.Add((D, Point, Random) =>
+            {
+                if (D.GetTile(Point) == 1)
+                    return TileTransformer.CreateTransform(CityTile.Road, 2);
+
+                return null;
+            });
+
+            // Building
+            TileTransformer.Lambdas.Add((D, Point, Random) =>
+            {
+                if (D.GetTile(Point) == 2)
+                    return TileTransformer.CreateTransform(CityTile.Building, 3);
+
+                return null;
+            });
+
+            // Grass
+            TileTransformer.Lambdas.Add((D, Point, Random) =>
+            {
+                if (D.GetTile(Point) == 0)
+                    return TileTransformer.CreateTransform(CityTile.Grass, 1);
+
+                return null;
+            });
+
+            // Wall
+            TileTransformer.Lambdas.Add((D, Point, Random) =>
+            {
+                if (D.GetTile(Point) == 0 && D.Adjacent4(Point, 2))
+                    return TileTransformer.CreateTransform(CityTile.Wall, 3);
+
+                return null;
+            });
+
+            // Tree Trunk
+            TileTransformer.Lambdas.Add((D, Point, Random) =>
+            {
+                if (Random.NextByte() < 5 && D.NumWithin(Point, 6, 1, 2) == 0)
+                    return TileTransformer.CreateTransform(CityTile.TreeTrunk, 2);
+
+                return null;
+            });
 
 
-            label1.Text = String.Format("{0} roads", Generator.Roads);
+            // ----------------
+            // Foliage Transforms
+            FoliageTransformer.Lambdas.Add((D, Point, Random) =>
+            {
+                if (D.GetTile(Point) != CityTile.TreeTrunk && D.NumWithin(Point, 2, CityTile.TreeTrunk) > 0)
+                    return FoliageTransformer.CreateTransform(CityTile.Foliage, 2);
 
+                return null;
+            });
+
+            FoliageTransformer.Lambdas.Add((D, Point, Random) =>
+            {
+                return FoliageTransformer.CreateTransform(D.GetTile(Point), 1);
+            });
+
+
+            //--------------
+            // Set up the lambdas that draw the thing
+
+            // Roads
+            MapTransformer.Lambdas.Add((D, Point, Random) =>
+            {
+                if (D.GetTile(Point) == CityTile.Road)
+                    return MapTransformer.CreateTransform(Color.SandyBrown, 3);
+
+                return null;
+            });
+
+            // Grass
+            MapTransformer.Lambdas.Add((D, Point, Random) =>
+            {
+                if (D.GetTile(Point) == CityTile.Grass)
+                    return MapTransformer.CreateTransform(Color.ForestGreen, 1);
+
+                return null;
+            });
+
+            // Foliage
+            MapTransformer.Lambdas.Add((D, Point, Random) =>
+            {
+                if (D.GetTile(Point) == CityTile.Foliage)
+                    return MapTransformer.CreateTransform(Color.DarkGreen, 1);
+
+                return null;
+            });
+
+            // Tree Trunk
+            MapTransformer.Lambdas.Add((D, Point, Random) =>
+            {
+                if (D.GetTile(Point) == CityTile.TreeTrunk)
+                    return MapTransformer.CreateTransform(Color.OrangeRed, 1);
+
+                return null;
+            });
+
+            // Building Walls
+            MapTransformer.Lambdas.Add((D, Point, Random) =>
+            {
+                if (D.GetTile(Point) == CityTile.Wall)
+                    return MapTransformer.CreateTransform(Color.Black, 3);
+
+                return null;
+            });
+
+            // Building Floor
+            MapTransformer.Lambdas.Add((D, Point, Random) =>
+            {
+                if (D.GetTile(Point) == CityTile.Building)
+                    return MapTransformer.CreateTransform(Color.SlateGray, 2);
+
+                return null;
+            });
+
+            //-------------
+
+            TileTransformer.Transform();
+            FoliageTransformer.Transform();
+            MapTransformer.Transform();
+
+            label1.Text = String.Format("{0} roads, {1} rooms, {2} attempts", Generator.Roads, Generator.Rooms, Attempts);
             Bitmap T = new Bitmap(Dungeon.Size.Width, Dungeon.Size.Height, PixelFormat.Format32bppArgb);
 
             for (int X = 0; X < Dungeon.Size.Width; X++)
             {
                 for (int Y = 0; Y < Dungeon.Size.Height; Y++)
                 {
-                    T.SetPixel(X, Y, Palette[Dungeon.GetTile(X, Y)]);
+                    T.SetPixel(X, Y, ColourMap.GetTile(X, Y));
                 }
             }
 
             pictureBox1.Image = T;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
